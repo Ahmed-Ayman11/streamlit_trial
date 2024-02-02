0630ca4d02ed4db17627186602ee87eb602a01e0
Ahmed-Ayman11 <143191323+Ahmed-Ayman11@users.noreply.github.com>
2024-02-02 15:19:18 +0200

    Add files via upload

---
+++ displaytext.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Hello World')
+st.header('this is the  header')
+st.markdown('hi is the markdown')
+st.subheader('hi is the  subheader')
+st.caption('this is the caption')
+st.code('''if x == 5:
+        print('hello world')''')
+st.latex(r'''a+a r^1+a r^2+a r^3''')
+st.image("https://pbs.twimg.com/ext_tw_video_thumb/1270730018600226817/pu/img/N1nn4BOpYxURhlxS.jpg")
+st.video("D:\yrryufuereru\\fadel.mp4")
